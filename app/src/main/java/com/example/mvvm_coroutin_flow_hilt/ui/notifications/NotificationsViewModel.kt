3a709b932510a4a1459455078f264732847acf63
choijw9691 <choijw9691@gmail.com>
2022-07-21 00:53:29 +0900

    202207210053

---
+++ app/src/main/java/com/example/mvvm_coroutin_flow_hilt/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.mvvm_coroutin_flow_hilt.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
